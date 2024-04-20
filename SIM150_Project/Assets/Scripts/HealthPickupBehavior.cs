2db53ac9bdbb3292ff6ec9bd0f2a385deae1a1d3
Joseph Neubert <joeyfneubert@gmail.com>
2024-04-20 16:58:56 -0700

    Added jumping, game manager, GUI.

--- SIM150_Project/Assets/Scripts/HealthPickupBehavior.cs
+++ SIM150_Project/Assets/Scripts/HealthPickupBehavior.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class HealthPickupBehavior : MonoBehaviour
 {
+    public GameBehavior gameManager;
+
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameBehavior>();
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.name == "Player")
         {
             Destroy(this.transform.parent.gameObject);
             Debug.Log("Item collected!");
+
+            gameManager.Items += 1;
+            gameManager.HP += 1;
         }
     }
 }

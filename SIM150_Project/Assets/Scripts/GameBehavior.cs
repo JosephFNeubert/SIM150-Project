b9dd5ea2d18f18bf9c77d67fd7713deafb3ffc8b
Joseph Neubert <joeyfneubert@gmail.com>
2024-04-28 12:12:58 -0700

    Added new OOP types (classes and interfaces)

--- SIM150_Project/Assets/Scripts/GameBehavior.cs
+++ SIM150_Project/Assets/Scripts/GameBehavior.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using CustomExtensions;
 
-public class GameBehavior : MonoBehaviour
+public class GameBehavior : MonoBehaviour, IManager
 {
+    private string _state;
+    public string State
+    {
+        get { return _state; }
+        set { _state = value; }
+    }
+
     public bool showWinScreen = false;
     public bool showLossScreen = false;
 
@@ -46,10 +54,16 @@ public class GameBehavior : MonoBehaviour
         }
     }
 
-    void RestartLevel()
+    public void Initialize()
     {
-        SceneManager.LoadScene(0);
-        Time.timeScale = 1.0f;
+        _state = "Manager initialized...";
+        _state.FancyDebug();
+        Debug.Log(_state);
+    }
+
+    void Start()
+    {
+        Initialize();
     }
 
     void OnGUI()
@@ -62,7 +76,7 @@ public class GameBehavior : MonoBehaviour
         {
             if (GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 100), "You won!"))
             {
-                RestartLevel();
+                Utilities.RestartLevel(0);
             }
         }
 
@@ -70,7 +84,7 @@ public class GameBehavior : MonoBehaviour
         {
             if(GUI.Button(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200 ,100), "You lose..."))
             {
-                RestartLevel();
+                Utilities.RestartLevel();
             }
         }
     }
